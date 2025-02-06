63d5ebe7149a1485d3c29b139fa6214f96b80c6d
CollinB <cbrey@uwm.edu>
2025-02-06 14:15:50 -0600

    first commit, hopefully this works,

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Replace with your desired URL
+    else:
+        return render(request, 'accounts/login.html', {})
